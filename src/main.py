89c9a79c48b9f90c07759ed12f6c2b0513055728
Jorge A. Pérez Garay <jperez@sw1848.com>
2020-08-11 02:11:46 -0500

    Decoded text

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 # Resolve the problem!!
+import re
 
 
 def run():
     # Start coding here
-
+    with open('src/encoded.txt', mode='r', encoding='utf-8') as f:
+        result = ''.join(re.findall('[a-z]', f.read()))
+    print(result)
 
 if __name__ == '__main__':
     run()
